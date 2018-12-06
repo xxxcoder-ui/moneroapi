c6d8513d9dab45442ceb0db00cb458122c2df43b
ziyaad30 <xavierbaatjes@yahoo.com>
2018-12-06 19:13:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var http = require('http');
-
 var fs = require('fs');
-
 var getJSON = require('get-json');
 
 
 // Loading the index file . html displayed to the client
-
 var server = http.createServer(function(req, res) {
 
     fs.readFile('./index.html', 'utf-8', function(error, content) {
@@ -20,30 +17,31 @@ var server = http.createServer(function(req, res) {
 });
 
 
-// Loading socket.io
-
 var io = require('socket.io').listen(server);
 
 
-// When a client connects, we note it in the console
-
-io.sockets.on('connection', function (socket) {
-
-    console.log('A client is connected!');
-
-});
-
 var reward;
 var BlockHeight;
+var BlockDiff;
 var coded;
+var xmr;
 
+   function convert(n){
+       var [lead,decimal,pow] = n.toString().split(/e|\./);
+       return +pow <= 0
+       ? "0." + "0".repeat(Math.abs(pow)-1) + lead + decimal
+       : lead + ( +pow >= decimal.length ? (decimal + "0".repeat(+pow-decimal.length)) : (decimal.slice(0,+pow)+"."+decimal.slice(+pow)))
+    }
+	
 function GetStatus(){		
 getJSON('https://moneroblocks.info/api/get_stats', function(error, response){
  
     //console.log(error);
     // undefined
     BlockHeight = response.height;
+	BlockDiff = response.difficulty;
 	reward = (response.last_reward / 1000000000000).toFixed(3);
+	xmr = convert((1000000 / BlockDiff) * reward * 0.9);
     console.log(response);
 
 });
@@ -57,7 +55,7 @@ setInterval(function () {
 
 io.sockets.on('connection', function (socket) {
 
-   socket.emit('message', { BlockHeight: BlockHeight, reward: reward });
+   socket.emit('message', { BlockHeight: BlockHeight, reward: reward, xmrReward: xmr, ServerMessage: '<div class="alert alert-danger" role="alert">This is a danger alert—check it out!</div>' });
 
 });
 
@@ -65,9 +63,9 @@ io.sockets.on('connection', function (socket) {
 io.sockets.on('connection', (socket) => {
     setInterval(() => {
         //socket.emit('message', coded)
-		socket.emit('message', { BlockHeight: BlockHeight, reward: reward });
+		socket.emit('message', { BlockHeight: BlockHeight, reward: reward, xmrReward: xmr, ServerMessage: '<div class="alert alert-success" role="alert">This is a success alert—check it out!</div>' });
     }, 5 * 1000);
 });
 
 
-server.listen(8080);
+server.listen(8888);
